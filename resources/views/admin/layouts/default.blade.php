7ddfd0825dfefc4636b1bbc0412ea85f26b2df4a
aungmyozaw92 <aungmyozaw.gcc@gmail.com>
2016-12-13 08:29:40 +0630

    default.blade.php edit

--- resources/views/admin/layouts/default.blade.php
+++ resources/views/admin/layouts/default.blade.php
@@ -19,7 +19,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
     <head>
         <meta charset="utf-8" />
-        <title>Metronic | Dashboard</title>
+        <title>MM Entertainment | Dashboard</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta content="" name="description" />
@@ -135,8 +135,6 @@ License: You must have a valid license purchased only from themeforest(the above
         {!!Html::script('backend/assets/global/plugins/morris/raphael-min.js')!!}
         {!!Html::script('backend/assets/global/plugins/counterup/jquery.waypoints.min.js')!!}
         {!!Html::script('backend/assets/global/plugins/counterup/jquery.counterup.min.js')!!}
-
-     
    
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN THEME GLOBAL SCRIPTS -->
